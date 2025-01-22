8ead55c08a9f7c4c289e6994bacb9b6e3c9d4036
Luca <luca.franke21+github@gmail.com>
2025-01-22 22:43:46 +0100

    add gateway

---
+++ api-gateway/src/main/kotlin/demo/microkernel/apigateway/Routes.kt
@@ -0,0 +1,28 @@
+package demo.microkernel.apigateway
+
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.cloud.gateway.route.builder.routes
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Routes {
+    @Bean
+    fun clientPortal(routeLocatorBuilder: RouteLocatorBuilder) =
+        routeLocatorBuilder.routes {
+            route(id = "shipping-service") {
+                path("/api/shipping/**")
+                uri("http://localhost:8083")
+            }
+            route(id = "payment-service") {
+                path("/api/payments/**")
+                uri("http://localhost:8082")
+            }
+            route(id = "order-service") {
+                path("/api/orders/**")
+                uri("http://localhost:8081")
+            }
+
+        }
+
+}
